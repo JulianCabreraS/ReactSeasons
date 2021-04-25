03cb54591e22c591dcb019ab95d5e670d1de1307
jcabrera <julian.cabreras@gmail.com>
2021-04-24 19:11:41 -0500

    2. Section 5 Sttate in React components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    //This is the only time we do direct assignment
+    this.state = { lat: null, errorMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        //We call setstate
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Latitud: </div>;
+  }
+
+  //* React says we have to define render
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>latitude:{this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
 
